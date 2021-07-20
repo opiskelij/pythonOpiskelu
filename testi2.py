7559905ab15b89a5b753d8069d4108fb323d7971
opiskelij <85615903+opiskelij@users.noreply.github.com>
2021-07-20 09:35:40 +0300

    Add files via upload

--- testi2.py
+++ testi2.py
@@ -1,5 +1,9 @@
 import random
+print("""
 
+-----------------------------------------------
+
+""")
 teksti = """Ehken kaikella tällä on oma merkityksensä,
 mutta silti, tämä on se asia mikä tässä viehättää.
 Ei toki kaikkea kaiken aikaa, mutta toki tiettyjä
@@ -28,6 +32,32 @@ Toimiiko Tämäkin
 
 print(teksti.format(testiä))
 
+#########################################################################
+print("""
+
+-----------------------------
+
+""")
+
+tassa = 500
+tassa2 = "Jotain tekstiä"
+tassa3 = "Kerron vielä kertaalleen"
+omateksti = """
+
+Tähän kirjoitan pienen koodin  {}  Josta voisin jotain oppia
+ja silti, jatkan vielä jotta voisin lisätä  {} tähän ja vielä toistamiseen
+koitan {} ja edelleen.
+
+"""
+
+print(omateksti.format(tassa2, tassa, tassa3))
+print("""
+
+-------------------------------
+
+
+""")
+##########################################################################
 print(omakommandi)
 
 testi1 = "Tämä voisi jopa toimiakkin"
@@ -51,7 +81,11 @@ s = 2
 
 print(i + o)
 
+print("""
 
+--------------------------------
+
+""")
 
 print(random.randrange(1,30))
 
@@ -72,3 +106,45 @@ if "edelleen" in g:
 if "Tässä" not in g:
     print("Lausetta tässä ei löytynyt")
 #print(random.randrange(1,10))
+
+
+print("""
+-------------------------------------
+""")
+
+tama = "On tekstiä"
+tama2 = "Osa tekstiä myös"
+tama3 = 4
+tama4 = 20
+
+tassa5 = "Pieni kirjotelma, jos se onnistuu {} mitä \" Koe \" kirjoitan ja\n tämä on {}, jota kirjoitan edelleen {}\ntoden sanoo ja ehken vielä {}"
+
+print(tassa5.format(tama4, tama2, tama3, tama))
+
+
+print("""
+-----------------------------------
+""")
+
+teksti1 = "Kokeillaan, montako merkkiä mahtuu yhteen, toiseen ja yhteen ja kolmanteen"
+merkki = teksti1.count("yhteen")
+
+print(merkki)
+
+print(teksti1.index("m")) #missä sijaitsee m kirjain
+
+print(teksti1.index("yhteen")) #kertoo missä kyseinen sana on
+
+print("""
+
+--------------------------------------
+
+""")
+
+tekstiä1 = "Kokeillaan taas toista kertaa miten homma toimisi"
+tekstiä2 = "Yhteenkirjoitettuna"
+test1 = tekstiä1.isalnum() #false koska välilyönnit
+testi2 = tekstiä2.isalnum() #toimii koska Yhteenkirjoitettuna
+
+print(test1) #false
+print(testi2) #true
